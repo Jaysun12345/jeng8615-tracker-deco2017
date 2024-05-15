f9cce2a48571bc4386c45825e7d6158f019593c7
Jaysun12345 <jasoneng_@hotmail.com>
2024-05-15 20:41:16 +1000

    finished some of the main page
    Finished newly released eps and am now thinking about how to put the graph into the html. Might use a plugin

---
+++ public/script.js
@@ -0,0 +1,17 @@
+// Function to get the current date and time
+function getCurrentDateAndTime() {
+    const weekdayArray = ["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"];
+    const monthArray = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"];
+    const date = new Date();
+    let weekday = weekdayArray[date.getDay()];
+    let day = date.getDate();
+    let month = monthArray[date.getMonth()];
+    let year = date.getFullYear();
+    let currentDate = weekday + ", " + day + " " + month + ", " + year;
+    return currentDate.toLocaleString();
+  }
+  
+// Target an HTML element to display the current date and time
+const dateDisplay = document.getElementById("date-container");
+// Set the innerHTML of the element to the current date and time returned by the function
+dateDisplay.innerHTML = getCurrentDateAndTime();
