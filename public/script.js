d19f9ff52882b0af6c503968693528dd0b5dfcc0
Jaysun12345 <jasoneng_@hotmail.com>
2024-05-16 19:18:12 +1000

    index.html finished mostly
    Finished most of main page, may need to complete supporting section

--- public/script.js
+++ public/script.js
@@ -10,8 +10,40 @@ function getCurrentDateAndTime() {
     let currentDate = weekday + ", " + day + " " + month + ", " + year;
     return currentDate.toLocaleString();
   }
-  
 // Target an HTML element to display the current date and time
 const dateDisplay = document.getElementById("date-container");
 // Set the innerHTML of the element to the current date and time returned by the function
 dateDisplay.innerHTML = getCurrentDateAndTime();
+
+// var xValues = ["Sun","Mon","Tue","Wed","Thu","Fri","Sat"];
+// var yValues = [55, 49, 44, 24, 15];
+
+
+//Making the bar graph 
+var ctx = document.getElementById('myChart').getContext('2d');
+var myChart = new Chart(ctx, {
+    type: 'bar',
+    data: {
+        labels: ['Fri', 'Sat', 'Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', ' '],
+        datasets: [{
+            label: "Monitor your watching habits",
+            data: [30, 85, 65, 20, 50, 30, 55, 30, 0],
+            backgroundColor: '#FFA745',
+            borderWidth: 1,
+            barThickness: 40,
+            borderRadius: 50
+        }]
+    },
+    options: {
+        scales: {
+            y: {
+                beginAtZero: true
+            }
+        },
+        plugins: {
+            legend: {
+              display: false
+            }
+        }
+    }
+});
