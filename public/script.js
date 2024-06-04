488988fe94aab77b74e91b509ca6f68fa03c0e31
Jaysun12345 <jasoneng_@hotmail.com>
2024-06-04 17:44:43 +1000

    adding anime now

--- public/script.js
+++ public/script.js
@@ -105,7 +105,7 @@ let animeList = [
         ep1Title: "Ep 1: Someone's Dream",
         ep1Image: "https://live.staticflickr.com/65535/53754970309_063288006a.jpg",
         ep1Length: 23,
-        ep1Description: "An Upper Rank Demon has been defeated for the first time in a hundred years by Uzui, Tanjiro, and his comrades' effort. This news causes a ripple effect not only among the members of the Demon Slayer Corps, but also the Upper Rank Demons, who have been summoned by Muzan Kibutsuji.",
+        ep1Description: "An Upper Rank Demon has been defeated for the first time in a hundred years by Uzui, Tanjiro, and his comrades' effort. This news causes a ripple effect not only among the members of the Demon Slayer Corps, but also the Upper Rank Demons.",
         ep2Title: "Ep 2: Yoriichi Type Zero",
         ep2Image: "https://live.staticflickr.com/65535/53754838348_dd330ab102.jpg",
         ep2Length: 23,
@@ -126,12 +126,75 @@ let animeList = [
         ep1Title: "Ep 1: The Other Side of the Sea",
         ep1Image: "https://live.staticflickr.com/65535/53756747408_87fd6ab641_c.jpg",
         ep1Length: 24,
-        ep1Description: "Marley besieges Fort Slava, aiming to end their four-year conflict with the Mid-East Allied Forces sparked by their defeat in Shiganshina. Warrior candidate Falco Grice is saved by his brother Colt, who returns him to the trenches nearby.",
+        ep1Description: "Marley besieges Fort Slava, aiming to end their four-year conflict with the Mid-East Allied Forces sparked by their defeat in Shiganshina.",
         ep2Title: "Ep 2: Midnight Train",
         ep2Image: "https://live.staticflickr.com/65535/53756547871_b85a69fd36_c.jpg",
         ep2Length: 24,
         ep2Description: "It is clear to Marley's military leaders that the era of their country's domination via Titan warfare is coming to an end. Marley plans to revive the Paradis Island Operation to capture the Founding Titan."
-    }
+    },
+    {
+        id: 4,
+        title: "Hunter X Hunter",
+        year: "2011",
+        seasonAndEps: "S 1 • EPS 26",
+        category1: "Fantasy",
+        category2: "Adventure",
+        description: "Gon Freecss is on a mission to train himself as a hunter just like his father. He reunites with his father, who is alive and an accomplished hunter too.",
+        categoryTag1: "https://live.staticflickr.com/65535/53754482194_6493806959.jpg",
+        categoryTag2: "https://live.staticflickr.com/65535/53754349228_5e1db76bb2.jpg",
+        image: "https://live.staticflickr.com/65535/53767445917_21d2679b58_n.jpg",
+        alt: "Hunter x hunter",
+        ep1Title: "Ep 1: Departure x and x Friends",
+        ep1Image: "https://live.staticflickr.com/65535/53768797480_c352dae71c_c.jpg",
+        ep1Length: 23,
+        ep1Description: "Young Gon must prove his ability to his aunt Mito before he's allowed to follow in his father's footsteps and take the Hunter Exam.",
+        ep2Title: "Ep 2: Test x of x Tests",
+        ep2Image: "https://live.staticflickr.com/65535/53768797470_782ea19745.jpg",
+        ep2Length: 23,
+        ep2Description: "On their way to take the Hunter Exam, Gon, Leorio, and Kurapika must pass an old woman's test and answer a question correctly."
+    },
+    {
+        id: 5,
+        title: "Food Wars",
+        year: "2015",
+        seasonAndEps: "S 1 • EPS 24",
+        category1: "Comedy",
+        category2: "Drama",
+        description: "Soma Yukihira battles some of the world's best chefs to prove himself and refine his skills.",
+        categoryTag1: "https://live.staticflickr.com/65535/53754349183_e0ee3af035.jpg",
+        categoryTag2: "https://live.staticflickr.com/65535/53754482204_3f60e2d93f_w.jpg",
+        image: "https://live.staticflickr.com/65535/53768381141_538e984d2d_w.jpg",
+        alt: "Food wars",
+        ep1Title: "Ep 1: An Endless Wasteland",
+        ep1Image: "https://live.staticflickr.com/65535/53768797435_847218bed9.jpg",
+        ep1Length: 25,
+        ep1Description: "Soma constantly battles his father Joichiro to see who is the best chef at their family restaurant. When his father goes away, an urban developer seeks to get rid of the restaurant.",
+        ep2Title: "Ep 2: God Tongue",
+        ep2Image: "https://live.staticflickr.com/65535/53768797450_40052945ee_c.jpg",
+        ep2Length: 25,
+        ep2Description: "Soma cooks against Erina, the owner of the God's Tongue. During this challenge, Soma must alter his plans and puts together a superior egg dish."
+    },
+    {
+        id: 6,
+        title: "Apothecary Diaries",
+        year: "2023",
+        seasonAndEps: "S 1 • EPS 24",
+        category1: "Romance",
+        category2: "Drama",
+        description: "Maomao, an apothecary's daughter, has been plucked from her peaceful life and sold to the lowest echelons of the imperial court. Now merely a maid, Maomao settles into her new mundane life and hides her extensive knowledge of medicine in order to avoid any unwanted attention.",
+        categoryTag1: "https://live.staticflickr.com/65535/53754574045_995985e51a.jpg",
+        categoryTag2: "https://live.staticflickr.com/65535/53754482204_3f60e2d93f_w.jpg",
+        image: "https://live.staticflickr.com/65535/53768552248_30898dcc60_w.jpg",
+        alt: "Apothecary Diaries",
+        ep1Title: "Ep 1: Maomao",
+        ep1Image: "https://live.staticflickr.com/65535/53768591698_01bf8bc0ef.jpg",
+        ep1Length: 23,
+        ep1Description: "Maomao becomes an imperial palace maid and investigates a rumored curse causing the princes' deaths, spurred by her insatiable curiosity when she overhears two consorts arguing about medicine and poison",
+        ep2Title: "Ep 2: Buaisô na kusushi",
+        ep2Image: "https://live.staticflickr.com/65535/53767483707_f14a35b44f.jpg",
+        ep2Length: 23,
+        ep2Description: "Maomao, who became the High Consort Gyokuyou's pharmaceutical attendant and poison tester, is tasked by Jinshi to create an unusual medicine."
+    },
 ];
 
 document.addEventListener('DOMContentLoaded', () => {
@@ -139,7 +202,7 @@ document.addEventListener('DOMContentLoaded', () => {
     let viewVault = document.querySelector(".view-vault-cards");
 
     // Retrieve the stored view vault cards from localStorage
-    viewVault.innerHTML = localStorage.getItem("viewVaultCards") || "";
+    viewVault.innerHTML = localStorage.getItem("viewVaultCards");
 
     // Function to display anime cards
     function displayAnimeCards(animeList) {
@@ -148,7 +211,7 @@ document.addEventListener('DOMContentLoaded', () => {
             // Creating the cards for each anime 
             let card = document.createElement("div");
             card.setAttribute("class", "anime-button-div");
-            card.setAttribute("data-anime-id", anime.id) // Setting each anime card with their own id
+            card.setAttribute("data-anime-id", anime.id); // Setting each anime card with their own id
 
             // Each anime card is a button that has the image, title, season, and eps within it
             let button = document.createElement("button");
@@ -179,6 +242,7 @@ document.addEventListener('DOMContentLoaded', () => {
             // Creating the popup
             let popup = document.createElement("div");
             popup.setAttribute("class", "popup");
+            popup.setAttribute("data-popup-id", anime.id);
             popup.style.display = "none";
 
             // Creating an overlay
@@ -202,8 +266,12 @@ document.addEventListener('DOMContentLoaded', () => {
                         <img src="${anime.categoryTag2}" alt="Category Tag 2"/>         
                     </div>
                     <button class="add-to-watchlist" data-anime-id="${anime.id}">
-                        <img class="add-button" src="https://live.staticflickr.com/65535/53754160146_b10bb221c6_s.jpg" alt="plus symbol"/>
+                        <img class="add-button" src="https://live.staticflickr.com/65535/53754160146_b10bb221c6_m.jpg" alt="plus symbol"/>
                         <span>Add to ViewVault</span>
+                    </button>
+                    <button class="remove-from-viewvault" style="display:none; margin-top:10px; margin-left:-10px;" data-anime-id="${anime.id}">
+                        <img class="remove-button" style="height:35px; margin-top:4px;" src="https://live.staticflickr.com/65535/53766019078_20ac210e42_m.jpg" alt="minus symbol"/>
+                        <span>Remove from ViewVault</span>
                     </button>
                 </div>
                 <img class="popup-display-image" src="${anime.image}" alt="${anime.alt}"/>
@@ -266,12 +334,6 @@ document.addEventListener('DOMContentLoaded', () => {
             card.appendChild(popup);
             card.appendChild(overlay);
 
-            // Show popup and overlay when the card is clicked
-            card.addEventListener("click", function () {
-                popup.style.display = "block";
-                overlay.style.display = "block";
-            });
-
             // Prevent closing popup when clicking inside it
             popup.addEventListener("click", function (event) {
                 event.stopPropagation();
@@ -284,19 +346,56 @@ document.addEventListener('DOMContentLoaded', () => {
             });
             animeCards.appendChild(card);
         });
+        // Attach event listeners to new anime cards
+        attachCardListeners();
     }
     
     displayAnimeCards(animeList); // Initial display of all anime cards
-    
-    // When the "Add to ViewVault" button is clicked, anime card is added to ViewVault
-    const addToViewVault = document.querySelectorAll(".add-to-watchlist");
-    for (let button of addToViewVault) {
-        button.addEventListener("click", function(event) {
-            let animeId = event.currentTarget.dataset.animeId; // Identifying each card by their id and appending it to ViewVault
-            let popupCard = document.querySelector(`[data-anime-id="${animeId}"].anime-button-div`);
-            viewVault.appendChild(popupCard);
-            localStorage.setItem("viewVaultCards", viewVault.innerHTML);
-        });
+
+    // Function to attach event listeners to anime cards
+    function attachCardListeners() {
+        let allAnime = document.querySelectorAll(".anime-button-div");
+        for (let card of allAnime) {
+            card.addEventListener("click", function(event) {
+                let animeId = event.currentTarget.dataset.animeId; // Identifying each card by their id and appending it to ViewVault
+                let popupCard = document.querySelector(`[data-popup-id="${animeId}"].popup`);
+                let overlay = popupCard.nextElementSibling;
+                popupCard.style.display = "block";
+                overlay.style.display = "block";
+            });
+        }
+
+        const addToViewVault = document.querySelectorAll(".add-to-watchlist");
+        for (let button of addToViewVault) {
+            button.addEventListener("click", function(event) {
+                event.stopPropagation(); // Prevent triggering the card click event
+                // Adds the card to viewVault
+                let animeId = event.currentTarget.dataset.animeId; // Identifying each card by their id and appending it to ViewVault
+                let popupCard = document.querySelector(`[data-anime-id="${animeId}"].anime-button-div`);
+                viewVault.appendChild(popupCard);
+
+                // Add button display becomes none
+                let addButton = document.querySelector(`[data-anime-id="${animeId}"].add-to-watchlist`);
+                addButton.style.display = "none";
+
+                // Remove button appears 
+                let removeButton = document.querySelector(`[data-anime-id="${animeId}"].remove-from-viewvault`);
+                removeButton.style.display = "block";
+                localStorage.setItem("viewVaultCards", viewVault.innerHTML);
+            });
+        }
+
+        const removeFromViewvault = document.querySelectorAll(".remove-from-viewvault");
+        for (let button of removeFromViewvault) {
+            button.addEventListener("click", function(event) {
+                event.stopPropagation(); // Prevent triggering the card click event
+                let animeId = event.currentTarget.dataset.animeId; // Identifying each card by their id and appending it to ViewVault
+                let popupCard = document.querySelector(`[data-anime-id="${animeId}"].anime-button-div`);
+                // Removing the child card from viewvault
+                viewVault.removeChild(popupCard)
+                localStorage.setItem("viewVaultCards", viewVault.innerHTML);
+            });
+        }
     }
 
     // When the page is loaded, popup and overlay are display = none
@@ -304,33 +403,13 @@ document.addEventListener('DOMContentLoaded', () => {
     let overlay = document.querySelectorAll(".overlay");
     popup.forEach(function(el) {
         el.style.display = "none";
-    })
+    });
     overlay.forEach(function(el) {
         el.style.display = "none";
     });
-    // When the exit button is clicked on each popup, the popup and overlay disappears
-    let exitButton = document.querySelectorAll(".exit-button");
-    for (let button of exitButton) {
-        button.addEventListener("click", function(event) {
-            popup.forEach(function(el) {
-                el.style.display = "none";
-            })
-            overlay.forEach(function(el) {
-                el.style.display = "none";
-            });
-            let allAnime = document.querySelectorAll(".anime-button-div")
-            allAnime.addEventListener("click", function(event) {
-                popup.forEach(function(el) {
-                    el.style.display = "block";
-                })
-                overlay.forEach(function(el) {
-                    el.style.display = "block";
-                });
-                console.log("clicked");
-            })
-        });
-    }
 
+    // Attach event listeners to anime cards initially
+    attachCardListeners();
 
     // Search bar code
     const searchBar = document.getElementById('search-bar');
@@ -406,6 +485,7 @@ function updateAverage() {
     weeklyAverage.innerHTML = Math.round((30 + 85 + 65 + 20 + 50 + 30 + 55 + todayScreentime) / 8);
 }
 
+// Filter categories as radio buttons 
 const radioButtons = document.querySelectorAll('input[name="category"]');
 radioButtons.forEach(radioButton => {
     radioButton.addEventListener('change', function () {
@@ -414,6 +494,7 @@ radioButtons.forEach(radioButton => {
     });
 });
 
+// Display cards with selected filters 
 function filterAnimeCards(category) {
     const cards = document.querySelectorAll(".anime-button-div");
     cards.forEach(card => {
@@ -421,9 +502,24 @@ function filterAnimeCards(category) {
         const cardCategory2 = card.getAttribute("data-category2");
 
         if (category === "all" || cardCategory1 === category || cardCategory2 === category) {
-                card.style.display = "block";
+            card.style.display = "block";
         } else {
             card.style.display = "none";
         }
+        attachExitButtonListeners();
+    });
+}
+
+// Function for the exit buttons after refreshing the page
+function attachExitButtonListeners() {
+    let exitButtons = document.querySelectorAll(".exit-button");
+    exitButtons.forEach(button => {
+        button.addEventListener("click", function(event) {
+            event.stopPropagation(); // Prevent triggering the card click event
+            let popup = button.closest('.popup');
+            let overlay = popup.nextElementSibling;
+            popup.style.display = "none";
+            overlay.style.display = "none";
+        });
     });
 }
